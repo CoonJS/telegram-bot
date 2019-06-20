58beb69986e9f265ae9eeef258ff93c999cf6c64
CoonJS <r_babanov@manychat.com>
2019-06-20 16:03:49 +0300

    change request template

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const express = require('express');
 
 const token = fs.readFileSync('./token.txt');
 const chatId = '347227894';
-
 const text = 'HELLO WORLD';
-const requestURL = `https://api.telegram.org/bot${token}/sendMessage?chatId=g${chatId}&text=Hello + World`;
+
+const requestURL = `https://api.telegram.org/bot${token}/sendMessage?chat_id=-${chatId}&text=Hello + World`;
+console.log(requestURL, 'tok')
 
 const port = 443;
 const app = express();
@@ -18,6 +19,7 @@ const options = {
 app.get('/', (req, res) => {
     res.send("IT'S WORKING!")
 });
+
 
 https.createServer(options, app).listen(port, () => {
     console.log('server running at ' + port)
