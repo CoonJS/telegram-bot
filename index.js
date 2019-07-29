8e15ad404c62f82b5f0302e0530e762a15b8a5a7
CoonJS <r_babanov@manychat.com>
2019-07-29 13:35:26 +0300

    add express response

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const fs = require('fs')
 const token = require('./token')
 const express = require('express')
-
 const TelegramApiController = require('./controllers/TelegramAPI')
+
 const chatId = '347227894'
 const text = 'HELLO WORLD'
-console.log(token)
 
 const tmAPI = new TelegramApiController(token)
 
-tmAPI.sendMessage({ chatId, text }, (err, res, body) => {
-    console.log(body)
+const app = express()
+const port = 80
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+    tmAPI.sendMessage({ chatId, text }, (err, res, body) => {
+        console.log(body)
+    })
 })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
