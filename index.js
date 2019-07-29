7dfeefe7427b1d529c4096c1a5dbc981122af968
CoonJS <r_babanov@manychat.com>
2019-07-29 13:28:28 +0300

    change token file

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
+const token = require('./token')
 const express = require('express')
-const TelegramApiController = require('./controllers/TelegramAPI')
 
+const TelegramApiController = require('./controllers/TelegramAPI')
 const chatId = '347227894'
 const text = 'HELLO WORLD'
-const token = fs.readFileSync('./token.txt')
 console.log(token)
 
 const tmAPI = new TelegramApiController(token)
