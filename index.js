0b843477f80995553a939aa9b3617c19c902b436
CoonJS <r_babanov@manychat.com>
2019-07-29 13:19:53 +0300

    log token

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const TelegramApiController = require('./controllers/TelegramAPI')
 const chatId = '347227894'
 const text = 'HELLO WORLD'
 const token = fs.readFileSync('./token.txt')
+console.log(token)
 
 const tmAPI = new TelegramApiController(token)
 
