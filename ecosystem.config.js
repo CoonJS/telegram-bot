26fa3f7e9a6b658a3933e25d59c9516d94eca40e
CoonJS <r_babanov@manychat.com>
2019-08-05 18:35:34 +0300

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,10 +4,6 @@ module.exports = {
             name: 'tm-bot',
             script: './index.js',
             watch: true,
-            env: {
-                PORT: 80,
-                NODE_ENV: 'development',
-            },
             env_production: {
                 PORT: 443,
                 NODE_ENV: 'production',
