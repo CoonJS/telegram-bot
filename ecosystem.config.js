05c4e0a8bb6e507818dfb49776452557f0dc4c23
CoonJS <r_babanov@manychat.com>
2019-08-05 18:41:19 +0300

    add logs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,13 @@ module.exports = {
             name: 'tm-bot',
             script: './index.js',
             watch: true,
+            error_file: 'err.log',
+            out_file: 'out.log',
+            log_file: 'combined.log',
+            time: true,
+            env: {
+                NODE_ENV: 'development',
+            },
             env_production: {
                 PORT: 443,
                 NODE_ENV: 'production',
